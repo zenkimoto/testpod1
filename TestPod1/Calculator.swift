55e095efe6fd8de6257a17b1d259be62c248d237
alexyip123 <alexyip123@gmail.com>
2017-08-17 20:54:06 -0700

    Initial commit

---
+++ TestPod1/Calculator.swift
@@ -0,0 +1,23 @@
+//
+//  Calculator.swift
+//  TestPod1
+//
+//  Created by Lai Yip on 8/17/17.
+//  Copyright © 2017 Lai Yip. All rights reserved.
+//
+
+import Foundation
+
+public class Calculator {
+    public func add(op1: Int, op2: Int) -> Int {
+        return op1 + op2
+    }
+    
+    public func subtract(op1: Int, op2: Int) -> Int {
+        return op1 - op2
+    }
+    
+    public func multiply(op1: Int, opt2: Int) -> Int {
+        return op1 * op2
+    }
+}
---
+++ TestPod1/TestPod1.h
@@ -0,0 +1,19 @@
+//
+//  TestPod1.h
+//  TestPod1
+//
+//  Created by Lai Yip on 8/17/17.
+//  Copyright © 2017 Lai Yip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestPod1.
+FOUNDATION_EXPORT double TestPod1VersionNumber;
+
+//! Project version string for TestPod1.
+FOUNDATION_EXPORT const unsigned char TestPod1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPod1/PublicHeader.h>
+
+
